cd4ae3abd22533585936b579fabde6ba5629ca04
Sandeep K <blackgen629@gmail.com>
2023-07-21 05:21:55 +0530

    v0.2.3a --changed back to img from Image

--- components/cards/collections-nft-cards/NftCard.tsx
+++ components/cards/collections-nft-cards/NftCard.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { INFT } from '../../../interfaces';
-import Image from 'next/image';
 
 const NFTCard: React.FC<INFT> = ({ name, imageUrl, externalUrl, shortURL, attributes }) => {
   
   return (
     <div className="bg-gray-300 overflow-hidden shadow rounded-lg p-4 m-2">
-      <Image src={imageUrl} alt={name} className="w-full h-64 object-cover mb-4" />
+      <img src={imageUrl} alt={name} className="w-full h-64 object-cover mb-4" />
       <div className="font-bold text-xl mb-2">{name}</div>
       <a href={externalUrl} target="_blank" rel="noopener noreferrer">External Link</a>
       <p className="text-grey-darker text-base">{shortURL}</p>
