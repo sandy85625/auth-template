5a1c25b181600e3f0384e268a223deeae7f559fe
Sandeep K <blackgen629@gmail.com>
2023-07-21 04:47:56 +0530

    v0.2.2 --added SEO

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,22 @@ import type { AppProps } from 'next/app';
 import withAuth from '../hocs/withAuth';
 import { AUTH_EXCEPTED_PUBLIC_URLS } from '../constants';
 import ErrorPage from '../components/errors/ErrorPage';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
   try {
-    return withAuth(Component, AUTH_EXCEPTED_PUBLIC_URLS)(pageProps);
+    return (
+      <>
+      <Head>
+      <title>Tickets App</title>
+        <meta name="description" content="A platform for creating loyality programs for businesses." />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta charSet="utf-8" />
+        <link rel="icon" href="/logo.png" />
+      </Head>
+      {withAuth(Component, AUTH_EXCEPTED_PUBLIC_URLS)(pageProps)}
+      </>
+      )
   } catch (error: any) {
     return <ErrorPage error={`${error.message}`} />
   }
